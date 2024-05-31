13274cbe5a6406ab1f9db00d06da4d81ee411976
Aditya Kumar <adityapal1409@gmail.com>
2024-05-31 12:25:47 +0530

    created watch Video

---
+++ src/components/WatchVideo.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeSidebar } from "../utils/slices/appSlice";
+import { useSearchParams } from "react-router-dom";
+
+const WatchVideo = () => {
+	const dispatch = useDispatch();
+	const [videoId] = useSearchParams();
+	const id = videoId.get("v");
+
+	useEffect(() => {
+		dispatch(closeSidebar());
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<iframe
+			width="1000"
+			height="600"
+			src={"https://www.youtube.com/embed/" + id}
+			title="YouTube video player"
+			frameBorder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			referrerPolicy="strict-origin-when-cross-origin"
+			allowFullScreen></iframe>
+	);
+};
+
+export default WatchVideo;
